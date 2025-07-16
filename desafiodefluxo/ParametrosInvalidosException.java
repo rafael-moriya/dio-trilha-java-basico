5b995631af9f2a32fb429421c8328f6d5792bec0
rafael-moriya <r.moriya300503@gmail.com>
2025-07-16 14:49:38 -0300

    desafio de fluxo

---
+++ desafiodefluxo/ParametrosInvalidosException.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package desafiocontrolefluxo;
+
+/**
+ *
+ * @author Rafael
+ */
+
+  public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
+
+
